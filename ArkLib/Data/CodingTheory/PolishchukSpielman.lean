14ea17c9e5a8e5b158f0b2de7a04018a02aa2f46
Ilia Vlasov <elijah.vlasov@gmail.com>
2025-08-27 16:04:51 +0100

    Blueprint for Guruswami-Sudan (#81)
    * Weighted degree
    
    * Root multiplicity
    
    * WIP
    
    * Decoder spec
    
    * WIP
    
    * fix typeclass inference
    
    * Guruswami-Sudan blueprint.
    
    * After rebase
    
    * Add proximity gap paper guruswami-sudan
    
    * Add non-zero condition into GS
    
    * Fix imports
    
    * Address comments
    
    * WIP
    
    * Addressed comments
    
    * codewordToPoly
    
    Co-authored-by: Frantisek Silvasi <silvasi.frantisek@gmail.com>
    
    * Expand a docstring
    
    ---------
    
    Co-authored-by: Frantisek Silvasi <silvasi.frantisek@gmail.com>

--- ArkLib/Data/CodingTheory/PolishchukSpielman.lean
+++ ArkLib/Data/CodingTheory/PolishchukSpielman.lean
@@ -17,7 +17,7 @@ lemma Polishchuk_Spielman {F : Type} [Semiring F] [Field F]
   (quot_X : F → F[X]) (quot_Y : F → F[X])
   (h_bx_ge_ax : b_x ≥ a_x) (h_by_ge_ay : b_y ≥ a_y)
   (h_f_degX : a_x ≥ Bivariate.degreeX f) (h_g_degX : b_x ≥ Bivariate.degreeX g)
-  (h_f_degY : a_y ≥ Bivariate.degreeY f) (h_g_degY : b_y ≥ Bivariate.degreeY g)
+  (h_f_degY : a_y ≥ natDegreeY f) (h_g_degY : b_y ≥ natDegreeY g)
   (h_card_Px : n_x ≤ P_x.card) (h_card_Py : n_y ≤ P_y.card)
   (h_le_1 : 1 > (b_x : ℚ)/(n_x : ℚ) + (b_y : ℚ)/(n_y : ℚ))
   (h_quot_X : ∀ y ∈ P_y,
@@ -25,7 +25,7 @@ lemma Polishchuk_Spielman {F : Type} [Semiring F] [Field F]
   (h_quot_Y : ∀ x ∈ P_x,
     (quot_Y x).natDegree ≤ (b_y - a_y) ∧ Bivariate.evalX x g = (quot_Y x) * (Bivariate.evalX x f))
   : ∃ q : F[X][Y], g = q * f
-    ∧ Bivariate.degreeX q ≤ b_x - a_x ∧ Bivariate.degreeY q ≤ b_y - a_y
+    ∧ Bivariate.degreeX q ≤ b_x - a_x ∧ natDegreeY q ≤ b_y - a_y
     ∧ (∃ Q_x : Finset F, Q_x.card ≥ n_x - a_x ∧ Q_x ⊆ P_x ∧
         ∀ x ∈ Q_x, Bivariate.evalX x q = quot_Y x)
     ∧ (∃ Q_y : Finset F, Q_y.card ≥ n_y - a_y ∧ Q_y ⊆ P_y ∧
--- ArkLib/Data/Polynomial/Bivariate.lean
+++ ArkLib/Data/Polynomial/Bivariate.lean
@@ -26,14 +26,65 @@ The set of coefficients of a bivariate polynomial.
 -/
 def coeffs [DecidableEq F] : Finset F[X] := f.support.image (fun n => f.coeff n)
 
-/-- The coeffiecient of `Y^n` is a polynomial in `X`.
+/-- (i, j)-coefficient of a polynomial, i.e. the coefficient
+    of `X^i Y^j`.
 -/
+def coeff (i j : ℕ) : F := (f.coeff j).coeff i
+
+/-- The coeffiecient of `Y^n`, as a polynomial in `X`. -/
 def coeff_Y_n (n : ℕ) : F[X] := f.coeff n
 
 /--
-The `Y`-degree of a bivariate polynomial.
+The `Y`-degree of a bivariate polynomial, as a natural number.
 -/
-def degreeY : ℕ := Polynomial.natDegree f
+def natDegreeY : ℕ := Polynomial.natDegree f
+
+/-- `(u,v)`-weighted degree of a polynomial.
+  The maximal `u * i + v * j` such that the polynomial `p`
+  contains a monomial `x^i * y * j`.
+-/
+def weightedDegree (p : F[X][Y]) (u v : ℕ) : WithBot ℕ :=
+  Finset.max (Finset.image (fun n => u * (p.coeff n).natDegree + v * n) p.support)
+
+/-- The weighted degree forced to a natural number. -/
+def natWeightedDegree (p : F[X][Y]) (u v : ℕ) : ℕ :=
+  Option.getD (weightedDegree p u v) 0
+
+/-- The finite set of bivariate indices of non-zero coefficients of
+    a bivariate polynomial.
+-/
+def support [DecidableEq F] (p : F[X][Y]) : Finset (ℕ × ℕ) :=
+  let deg := natWeightedDegree p 1 1
+  Finset.image 
+    (fun x => (x.1.val, x.2.val)) 
+    ({x : Fin deg.succ × Fin deg.succ | coeff p x.1 x.2 ≠ 0})
+
+
+/-- Root multiplicity of (0,0).
+    It is the minimal sum `i + j` over all `(i, j)` such that 
+    the (i,j)-coefficient of `f` is not zero.
+-/
+def rootMultiplicity₀ [DecidableEq F] : Option ℕ :=
+  Finset.min (Finset.image (fun x => x.1 + x.2) (support f))
+
+/-- Multiplicity of `(x, y)`. Defined as the multiplicity of `(0, 0)`
+    w.r.t. the polynomial `f(X + x, Y + y)`.
+-/
+def rootMultiplicity
+  {F : Type}
+  [CommSemiring F]
+  [DecidableEq F] (f : F[X][Y]) (x y : F) : Option ℕ :=
+  let X := (Polynomial.X : Polynomial F)
+  rootMultiplicity₀ (F := F) ((f.comp (Y + (C (C y)))).map (Polynomial.compRingHom (X + C x)))
+
+lemma rootMultiplicity_some_implies_root {F : Type} [CommSemiring F]
+  [DecidableEq F]
+  {x y : F} (f : F[X][Y])
+  (h : some 0 < (rootMultiplicity (f := f) x y))
+  :
+  (f.eval (Polynomial.C y)).eval x = 0
+  := by
+  sorry
 
 -- Katy: The next def, lemma and def can be deleted. Just keeping for now in case we need
 -- the lemma for somethying
@@ -84,8 +135,8 @@ Over an integral domain, the `Y`-degree of the product of two non-zero bivariate
 equal to the sum of their degrees.
 -/
 lemma degreeY_mul [IsDomain F] (hf : f ≠ 0) (hg : g ≠ 0)
-  : degreeY (f * g) = degreeY f + degreeY g := by
-  unfold degreeY
+  : natDegreeY (f * g) = natDegreeY f + natDegreeY g := by
+  unfold natDegreeY
   rw [←ne_zero_iff_leadingCoeffY_ne_zero] at hf
   rw [←ne_zero_iff_leadingCoeffY_ne_zero] at hg
   have h_lc : leadingCoeffY f * leadingCoeffY g ≠ 0 := mul_ne_zero hf hg
@@ -149,11 +200,11 @@ lemma nezero_iff_coeffs_nezero : f ≠ 0 ↔ f.coeff ≠ 0 := by
   apply Iff.intro
   · intro hf
     have f_finsupp : f.toFinsupp ≠ 0 := by aesop
-    rw [coeff]
+    rw [Polynomial.coeff]
     simp only [ne_eq, Finsupp.coe_eq_zero]
     exact f_finsupp
   · intro f_coeffs
-    rw [coeff] at f_coeffs
+    rw [Polynomial.coeff] at f_coeffs
     aesop
 
 /--
@@ -182,7 +233,7 @@ The `Y` degree of the bivarate quotient is bounded above by the difference of th
 the divisor and divident.
 -/
 lemma quotient_degY [IsDomain F] (q : F[X][Y]) (h_quot_XY : g = q * f) (hf : f ≠ 0)
-  (hg : g ≠ 0) : degreeY q  ≤ degreeY g - degreeY f := by
+  (hg : g ≠ 0) : natDegreeY q  ≤ natDegreeY g - natDegreeY f := by
   rw [h_quot_XY, degreeY_mul q f]
   · aesop
   · rw [nezero_iff_coeffs_nezero q]
