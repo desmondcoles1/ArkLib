446c51c9ef14bfd535d8a31e28834986f17d4c0e
Julian Sutherland <julian@nethermind.io>
2025-09-20 03:03:18 +0100

    Cleanup + doc strings for the  and  type classes

--- ArkLib/Data/GroupTheory/Smooth.lean
+++ ArkLib/Data/GroupTheory/Smooth.lean
@@ -5,27 +5,15 @@ import Mathlib.GroupTheory.SpecificGroups.Cyclic
   This avoids the use of Classical.choose to extract the generator,
   making dependent definitions incomputable.
 -/
-
 class IsCyclicWithGen (G : Type) [Pow G ℤ] where
   gen : G
   zpow_surjective : Function.Surjective (gen ^ · : ℤ → G)
 
+/- Derivation of an instance of Mathlib's `IsCyclic` from an instance if `IsCylicWithGen`. -/
 instance {G} [Pow G ℤ] [inst : IsCyclicWithGen G] : IsCyclic G where
   exists_zpow_surjective := ⟨inst.gen, inst.zpow_surjective⟩
 
--- class Smooth
---   (p : ℕ) [Fact (Nat.Prime p)]
---   (n : ℕ) (G : Type) [Pow G ℤ] [Monoid G] [inst : IsCyclicWithGen G] where
---   smooth : orderOf inst.gen = p ^ n
 
+/- Class indicating that a cyclic Monoid of order `2^n`. -/
 class SmoothPowerOfTwo (n : ℕ) (G : Type) [Pow G ℤ] [Monoid G] [inst : IsCyclicWithGen G] where
   smooth : orderOf inst.gen = 2 ^ n
-
--- instance {n : ℕ} {G : Type} [Pow G ℤ] [Monoid G] [inst : IsCyclicWithGen G] [inst : Smooth 2 n G] :
---   SmoothPowerOfTwo n G where
---   smooth := inst.smooth
-
--- instance {n : ℕ} {G : Type} [Pow G ℤ]  [Monoid G]
---   [inst : IsCyclicWithGen G] [inst : SmoothPowerOfTwo n G] :
---     Smooth 2 n G where
---   smooth := inst.smooth
